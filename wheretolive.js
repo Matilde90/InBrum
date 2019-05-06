d556074cf5c6a9e0a2205ddcd498ed7e1068f764
Matilde Aliffi <matilde.aliffi@gmail.com>
2019-05-06 13:49:27 +0100

    New navbar, added dropdown menu, responsive behaviour

--- wheretolive.js
+++ wheretolive.js
@@ -1,11 +1,30 @@
-function fadeIn(slow)
-{
-   $(this).fadeIn( fadeOut );
+//function fadeIn(slow)
+//{
+  // $(this).fadeIn( fadeOut );
+//}
+
+//function fadeOut()
+//{
+  // $(this).fadeOut( fadeIn );
+//}
+
+//fadeIn.call($("#sellyoak"));
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-function fadeOut()
-{
-   $(this).fadeOut( fadeIn );
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
-
-fadeIn.call($("#sellyoak"));
