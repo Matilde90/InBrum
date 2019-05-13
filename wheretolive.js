570c0006b87555f95fb6a8e846a3db3e6fd9df19
Matilde Aliffi <matilde.aliffi@gmail.com>
2019-05-13 19:03:01 +0100

    class commit

--- wheretolive.js
+++ wheretolive.js
@@ -1,9 +1,12 @@
 //logo animations
   $('.brum').on('mouseover', function() {
+    debugger;
+    console.log('i have mouse over');
       $(this).toggleClass('brum2');
   });
 
   $('.in').on('mouseover', function() {
+    console.log('i have mouse over');
       $(this).toggleClass('in2');
   });
 
