0c1278caeda9efe029c7ef2f17c19d4911560ab8
Matilde Aliffi <matilde.aliffi@gmail.com>
2019-05-14 18:26:54 +0100

    Changed pictures

--- wheretolive.js
+++ wheretolive.js
@@ -1,12 +1,10 @@
 //logo animations
+
   $('.brum').on('mouseover', function() {
-    debugger;
-    console.log('i have mouse over');
       $(this).toggleClass('brum2');
   });
 
   $('.in').on('mouseover', function() {
-    console.log('i have mouse over');
       $(this).toggleClass('in2');
   });
 
