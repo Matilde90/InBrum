9343e9bc43006d7171d338c93229bc59002a1e70
niranzri <silver_wyrd@hotmail.com>
2019-05-16 20:58:16 +0100

    updates noe

---
+++ whybham.js
@@ -0,0 +1,31 @@
+//logo animations
+
+  $('.brum').on('mouseover', function() {
+      $(this).toggleClass('brum2');
+  });
+
+  $('.in').on('mouseover', function() {
+      $(this).toggleClass('in2');
+  });
+
+function myFunction() {
+  document.getElementByClassName("ddown-content").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dbtn')) {
+    var dropdowns = document.getElementsByClassName("ddown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+//this shows the pic description when you click on the pic name
+  $('.bhampic').on('click', function() {
+      $(this).toggleClass('show-description');
+  });
